d865bb6a8970fef13152cb54b34a25fc5de536ed
Rumon <rumonuniv@gmail.com>
2021-12-23 21:17:48 +0900

    dino-run

--- dino.js
+++ dino.js
@@ -5,19 +5,29 @@ const DINO_FRAME_COUNT = 2;
 const FRAME_TIME = 100; // every second our frame should last 100 milisecond
 
 let isJumping;
+let dinoFrame;
+let currentFrameTime;
 export function setDino(){
     isJumping = false;
+    dinoFrame = 0;
 }
 
 export function updateDino(delta, speedScale){
-    handleRun();
+    handleRun(delta, speedScale);
     handleJump();
 }
 
-function handleRun(){
-    if(isJumping){
-        dinoElem.src = `images/dino-stationary.jpg`
+function handleRun(delta, speedScale){
+    if(isJumping){ // when jumping changing the dinosaur to stationary position
+        dinoElem.src = `images/dino-stationary.jpg`;
+        return;
     }
+    if(currentFrameTime >= FRAME_TIME){
+        dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT;
+        dinoElem.src = `img/dino-run-${dinoFrame}.png`;
+        currentFrameTime -= FRAME_TIME;
+    }
+    currentFrameTime += delta * speedScale
 }
 
 function handleJump(){
